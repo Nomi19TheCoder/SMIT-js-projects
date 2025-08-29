5e13ceb070b274ea47435aebe3d913d2d686360f
Muneeza4567 <Muneezamughal211@gmail.com>
2025-08-29 17:02:24 +0500

    add all

--- Code-2/script.js
+++ Code-2/script.js
@@ -1,34 +1,34 @@
-// JavaScript Assignment (Chapters 1–18)
-// 1. Ask the user to enter their age. If the age is less than 13, display 'Child'; if between 13 and 19,
-// display 'Teenager'; otherwise display 'Adult'.
+// // JavaScript Assignment (Chapters 1–18)
+// // 1. Ask the user to enter their age. If the age is less than 13, display 'Child'; if between 13 and 19,
+// // display 'Teenager'; otherwise display 'Adult'.
 let enterAge = prompt(`Enter Your Age`, `Weite Here`);
 const userAge = Number(enterAge);
 const message = userAge<13? `Child`: userAge >= 13 &&  userAge <= 19?`Teeneger`:`Adult`;
 let result=alert(message); 
 
-// 2. Write a program that asks the user to enter a number. If the number is divisible by both 2 and 3,
-// show 'Divisible by both'. Otherwise, show 'Not divisible by both'.
+// // 2. Write a program that asks the user to enter a number. If the number is divisible by both 2 and 3,
+// // show 'Divisible by both'. Otherwise, show 'Not divisible by both'.
 let enterNumber = prompt(`Enter Your Number`, `Weite Here`);
 const userNumber = Number(enterNumber);
 const messageSecond = userNumber%2===0 && userNumber%3===0 ?`Divisible by both`:`Not divisible by both`;
  result=alert(messageSecond); 
 
-// 3. Create a program that asks the user for a password. If it matches 'saylani123', show 'Access
-// Granted', otherwise show 'Access Denied'.
+// // 3. Create a program that asks the user for a password. If it matches 'saylani123', show 'Access
+// // Granted', otherwise show 'Access Denied'.
 const enterPassword=prompt('Enter Your Password', 'Enter Here')
 const userPassword = Number(enterPassword);
-const messageThird = userPassword==='saylani123'?'Access';'Access Denied'
+const messageThird = userPassword==='saylani123'?'Access':'Access Denied'
  result=alert(messageThird); 
-// 4. Ask the user to enter two numbers. Display the larger number using an if…else statement.
+// // 4. Ask the user to enter two numbers. Display the larger number using an if…else statement.
 const firstNumber= prompt('Enter a Number')
 const secondNumber= prompt('Enter an other Number')
 const firstEntered = Number(firstNumber);
 const secondEntered = Number(secondNumber);
 const messageFourth = Math.max(firstEntered,secondEntered);
  result=alert(`${messageFourth} is the larger number`); 
-// i read the question about using if else, but i found this method efficient way and i already use ternary method in the above question
+// // i read the question about using if else, but i found this method efficient way and i already use ternary method in the above question
 
-// 5. Create an array of 5 colors. Display the first and last color from the array in an alert.
+// // 5. Create an array of 5 colors. Display the first and last color from the array in an alert.
 const colorAdder=() => {
     const colorArr= [];
 for(i=0;i<5;i++){
@@ -46,8 +46,8 @@ result=alert(messageFifth);
 
 }
 
-// 6. Make an array of fruits with 3 items. Add one more fruit at the end using .push() and show the
-// new array in console.
+// // 6. Make an array of fruits with 3 items. Add one more fruit at the end using .push() and show the
+// // new array in console.
 const fruitArr=['apple','banana','mango'];
 const messageSixth=fruitArr.push('orange');
 console.log(`Array of fruit = ${fruitArr}`)
@@ -57,16 +57,16 @@ result=alert(fruitArr);
 const cityArr=['Karachi','Lahore','Islamabad'];
 cityArr.shift();
 console.log(cityArr);
-// 8. Create an array of numbers [10, 20, 30, 40, 50]. Use .splice() to remove the middle number (30)
-// and display the array.
+// // 8. Create an array of numbers [10, 20, 30, 40, 50]. Use .splice() to remove the middle number (30)
+// // and display the array.
 const numberArr = [10, 20, 30, 40, 50];
 numberArr.splice(2,1);
 console.log(numberArr);
-// 9. Make an array [100, 200, 300, 400, 500]. Use .slice(1, 4) and show the result in the console.
+// // 9. Make an array [100, 200, 300, 400, 500]. Use .slice(1, 4) and show the result in the console.
 const bignumberArr =[100, 200, 300, 400, 500];
 console.log(bignumberArr.slice(1,4));
-// 10. Ask the user to enter 3 subjects’ marks. Store them in an array. Calculate the total and average
-// using array values and display it.
+// // 10. Ask the user to enter 3 subjects’ marks. Store them in an array. Calculate the total and average
+// // using array values and display it.
 let entryValue =document.queryselector(`#entries`).textContent;
 const entries = Nummber(entryValue);
 const btn = document.querySelector(`button`);
@@ -82,85 +82,85 @@ const average =(entryArr[0],enteryArr[1],enteryArr[2])=>{
  return (entryArr[0] + entryArr[1] + entryArr[2])/3
 }
 result=average;
-// 11. Write a program that asks the user for a month number (1–12). Use if…else if statements to
-// display the season ('Winter', 'Spring', 'Summer', 'Autumn'). 
-const askMonth = prompt('Write the month Number you want to', 'Month Number')
-const monthNumber= Number(askMonth);
-const validation = monthNumber>=1 && monthNumber<=12;
-  if(validation){
-    const season=monthNumber<=3?'Winter':monthNumber<=6?'Autumn':monthNumber<=9?'Summer':monthNumber<=12?'December';
-    alert(`It's ${season}`);
-  }
-  else{
-  alert(`Your month number is not valid`);
-  }
-// 12. Create a loop that prints all even numbers from 2 to 20 in the console.
-const tableEven=()=>{
-  let evenArr=[]
-  for (i=0;i<11;i++){
-    const tableNumbers=i*2;
-    evenArr.push(tableNumbers);
-  }
-  return evenArr.join(',');
-} 
-console.log(tableEven())
-// 13. Use a for loop to display each element of the array ['Ali', 'Sara', 'Ahmed', 'Ayesha'].
-displayFunction=()=>{const nameArr=['Ali', 'Sara', 'Ahmed', 'Ayesha'];
-const diplayName=()=>{
-  for(i=0;i<nameArr.length;i++){
-    console.log(nameArr[i]);
-  };
-};
-};
-console.log(displayFunction());
-// 14. Write a program that asks the user to enter a number and then displays its multiplication table
-const  numberRrequir=prompt('Enter a Number for multiplication','0');
-const numberMultiple=Number(numberRrequir);
-const multiplicationArr=[];
-for (i=1;i=10;i++){
-  multiplicationArr.push(i*2);
-};
-alert(multiplicationArr);
+// // 11. Write a program that asks the user for a month number (1–12). Use if…else if statements to
+// // display the season ('Winter', 'Spring', 'Summer', 'Autumn'). 
+// const askMonth = prompt('Write the month Number you want to', 'Month Number')
+// const monthNumber= Number(askMonth);
+// const validation = monthNumber>=1 && monthNumber<=12;
+//   if(validation){
+//     const season=monthNumber<=3?'Winter':monthNumber<=6?'Autumn':monthNumber<=9?'Summer':monthNumber<=12?'December';
+//     alert(`It's ${season}`);
+//   }
+//   else{
+//   alert(`Your month number is not valid`);
+//   }
+// // 12. Create a loop that prints all even numbers from 2 to 20 in the console.
+// const tableEven=()=>{
+//   let evenArr=[]
+//   for (i=0;i<11;i++){
+//     const tableNumbers=i*2;
+//     evenArr.push(tableNumbers);
+//   }
+//   return evenArr.join(',');
+// } 
+// console.log(tableEven())
+// // 13. Use a for loop to display each element of the array ['Ali', 'Sara', 'Ahmed', 'Ayesha'].
+// displayFunction=()=>{const nameArr=['Ali', 'Sara', 'Ahmed', 'Ayesha'];
+// const diplayName=()=>{
+//   for(i=0;i<nameArr.length;i++){
+//     console.log(nameArr[i]);
+//   };
+// };
+// };
+// console.log(displayFunction());
+// // 14. Write a program that asks the user to enter a number and then displays its multiplication table
+// const  numberRrequir=prompt('Enter a Number for multiplication','0');
+// const numberMultiple=Number(numberRrequir);
+// const multiplicationArr=[];
+// for (i=1;i=10;i++){
+//   multiplicationArr.push(i*2);
+// };
+// alert(multiplicationArr);
 
-// using a loop (up to 10).
-// 15. Create an array [2, 4, 6, 8, 10]. Use a for loop to calculate the sum of all numbers and print it.
-const  myArr= [2, 4, 6, 8, 10];
-const addFunction=(primary,secondary)=>{
-  returm primary+secondary;
-}
-console.log(myArr.reduce(addFunction));
-// 16. Ask the user to enter a name. If the name is 'Alice' or 'Bob', show 'Welcome!'. Otherwise, show
-// 'You are not authorized.'
-const enterName=prompt('Enter a Name','Name');
-const enteredName=enterName.tolowercase()
-const nameFunction=()=>{
-  switch(enteredName){
-    case: 'bob';
-    alert(`Welcome!`);
-    break;
-    case: 'alice';
-    alert(`Welcome!`);
-    break;
-    default:
-    alert(`You are not authorized.`)
-  }
-}
-// 17. Write a program that uses nested if statements to check if a number is positive and also even. If
-// both are true, display 'Positive Even'. If only positive, display 'Positive Odd'. Otherwise show
-// 'Negative number'.
-const unknownName=prompt('Enter a number','number');
-const checkNumber=Number(unknownName);
-const nameFunction=()=>{
-  if (checkNumber>=0){
+// // using a loop (up to 10).
+// // 15. Create an array [2, 4, 6, 8, 10]. Use a for loop to calculate the sum of all numbers and print it.
+// const  myArr= [2, 4, 6, 8, 10];
+// const addFunction=(primary,secondary)=>{
+//   returm primary+secondary;
+// }
+// console.log(myArr.reduce(addFunction));
+// // 16. Ask the user to enter a name. If the name is 'Alice' or 'Bob', show 'Welcome!'. Otherwise, show
+// // 'You are not authorized.'
+// const enterName=prompt('Enter a Name','Name');
+// const enteredName=enterName.tolowercase()
+// const nameFunction=()=>{
+//   switch(enteredName){
+//     case: 'bob';
+//     alert(`Welcome!`);
+//     break;
+//     case: 'alice';
+//     alert(`Welcome!`);
+//     break;
+//     default:
+//     alert(`You are not authorized.`)
+//   }
+// }
+// // 17. Write a program that uses nested if statements to check if a number is positive and also even. If
+// // both are true, display 'Positive Even'. If only positive, display 'Positive Odd'. Otherwise show
+// // 'Negative number'.
+// const unknownName=prompt('Enter a number','number');
+// const checkNumber=Number(unknownName);
+// const nameFunction=()=>{
+//   if (checkNumber>=0){
     
-    const myNumber= checkNumber%2=1?'Possitive Even':checkNumber%2!=1?'Possitive Odd';
-    alert(`Your Number is ${myNumber}`)
-  }
-  else{
-    alert('Your Number is Negative')
-  };
-// 18. Create an array ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']. Use .splice() to insert 'Mango' at
-// the 2nd index and show the new array.
-let nextArr= ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
-nextArr.splice(1,1,'Mango');
-console.log(nextArr);
+//     const myNumber= checkNumber%2=1?'Possitive Even':checkNumber%2!=1?'Possitive Odd';
+//     alert(`Your Number is ${myNumber}`)
+//   }
+//   else{
+//     alert('Your Number is Negative')
+//   };
+// // 18. Create an array ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry']. Use .splice() to insert 'Mango' at
+// // the 2nd index and show the new array.
+// let nextArr= ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+// nextArr.splice(1,1,'Mango');
+// console.log(nextArr);
