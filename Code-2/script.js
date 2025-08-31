2ae2bdad5dc92fc05a0b1f733e09521277bd18c5
Noman Saeed <rfns19@gmail.com>
2025-09-01 00:07:31 +0500

    update Code-2/script.js

--- Code-2/script.js
+++ Code-2/script.js
@@ -137,8 +137,9 @@ alert(multiplicationArr);
 const  myArr= [2, 4, 6, 8, 10];
 const addFunction=(primary,secondary)=>{
   return primary+secondary;
-}
-console.log(myArr.reduce((addFunction(primary,secondary)));
+};
+let addition = myArr.reduce((addFunction));
+console.log(addition);
 // 16. Ask the user to enter a name. If the name is 'Alice' or 'Bob', show 'Welcome!'. Otherwise, show
 // // 'You are not authorized.'
 // const enterName=prompt('Enter a Name','Name');
